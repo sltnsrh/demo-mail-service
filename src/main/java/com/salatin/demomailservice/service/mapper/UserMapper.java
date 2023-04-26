e305002772ffe1f2e6af7616e12acd83276aba00
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-26 13:25:36 +0300

    UserMapper -> fixed setting a time to a User

--- src/main/java/com/salatin/demomailservice/service/mapper/UserMapper.java
+++ src/main/java/com/salatin/demomailservice/service/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.salatin.demomailservice.model.User;
 import com.salatin.demomailservice.model.dto.request.UserCreateRequestDto;
 import com.salatin.demomailservice.model.dto.request.UserUpdateRequestDto;
 import com.salatin.demomailservice.model.dto.response.UserResponseDto;
+import java.time.LocalDateTime;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,7 +12,7 @@ import org.mapstruct.Mapping;
 public interface UserMapper {
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdOn", expression = "LocalDateTime.now()")
+    @Mapping(target = "createdOn", expression = "java(getCurrentTime())")
     User toModel(UserCreateRequestDto requestDto);
 
     @Mapping(target = "id", ignore = true)
@@ -19,4 +20,8 @@ public interface UserMapper {
     User toModel(UserUpdateRequestDto requestDto);
 
     UserResponseDto toDto(User user);
+
+    default LocalDateTime getCurrentTime() {
+        return LocalDateTime.now();
+    }
 }
