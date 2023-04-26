83c71f79e6052405a0d10b99c8730faa7418a0e6
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-26 12:47:58 +0300

    UserController was created with mthd create()

---
+++ src/main/java/com/salatin/demomailservice/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.salatin.demomailservice.controller;
+
+import com.salatin.demomailservice.UserService;
+import com.salatin.demomailservice.model.dto.request.UserCreateRequestDto;
+import com.salatin.demomailservice.model.dto.response.UserResponseDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> create(@RequestBody @Valid UserCreateRequestDto requestDto) {
+
+        return null;
+    }
+}
