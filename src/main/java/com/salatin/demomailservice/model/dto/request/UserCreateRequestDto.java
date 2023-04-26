9ca87f3ebd9d3e4925063fa1fa9a0fc789972d54
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-26 15:06:59 +0300

    to UserCreateRequestDto validation was added

--- src/main/java/com/salatin/demomailservice/model/dto/request/UserCreateRequestDto.java
+++ src/main/java/com/salatin/demomailservice/model/dto/request/UserCreateRequestDto.java
@@ -1,9 +1,12 @@
 package com.salatin.demomailservice.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserCreateRequestDto {
+    @NotBlank
     private String username;
+    @NotBlank
     private String email;
 }
