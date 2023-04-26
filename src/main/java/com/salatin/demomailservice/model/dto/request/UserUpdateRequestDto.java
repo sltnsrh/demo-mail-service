c41e4098b7e69a8862f9d161d4043d9628ca1174
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-26 15:42:01 +0300

    added the field id to a UserUpdateRequestDto and fields validation

--- src/main/java/com/salatin/demomailservice/model/dto/request/UserUpdateRequestDto.java
+++ src/main/java/com/salatin/demomailservice/model/dto/request/UserUpdateRequestDto.java
@@ -1,9 +1,17 @@
 package com.salatin.demomailservice.model.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UserUpdateRequestDto {
+    @Min(value = 1, message = "The id value should be minimum 1")
+    @Max(value = Integer.MAX_VALUE, message = "The id value is too big")
+    private int id;
+    @NotBlank(message = "Username can't be blank")
     private String username;
+    @NotBlank(message = "Email can't be blank")
     private String email;
 }
