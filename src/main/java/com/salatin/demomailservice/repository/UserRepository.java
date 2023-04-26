231043fd3e4660bd954b8111a0535ebd2af847fe
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-26 14:09:38 +0300

    findByEmail and findByUsername methods were added to UserRepository

--- src/main/java/com/salatin/demomailservice/repository/UserRepository.java
+++ src/main/java/com/salatin/demomailservice/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.salatin.demomailservice.repository;
 
 import com.salatin.demomailservice.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
 }
