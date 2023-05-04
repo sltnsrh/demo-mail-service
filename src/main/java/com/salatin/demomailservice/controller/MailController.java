305369c582e5f617553a385eb879537f67ab9c8e
Serhii Salatin <lifenjoy8888@gmail.com>
2023-05-04 15:22:54 +0300

    were deleted Email, EmailSendRequestDto, EmailRepository, EmailService, EmailServiceImpl

--- src/main/java/com/salatin/demomailservice/controller/MailController.java
+++ src/main/java/com/salatin/demomailservice/controller/MailController.java
@@ -1,12 +1,8 @@
 package com.salatin.demomailservice.controller;
 
-import com.salatin.demomailservice.model.dto.request.EmailSendRequestDto;
-import com.salatin.demomailservice.service.MailSenderService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/mail")
 @RequiredArgsConstructor
 public class MailController {
-    private final MailSenderService mailSenderService;
 
     @PostMapping
-    public ResponseEntity<Void> send(@RequestBody @Valid EmailSendRequestDto requestDto) {
-        mailSenderService.send(requestDto.getTo(), requestDto.getSubject(), requestDto.getBody());
+    public ResponseEntity<Void> send() {
+
 
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/salatin/demomailservice/repository/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salatin.demomailservice.repository;
-
-import com.salatin.demomailservice.model.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailRepository extends JpaRepository<Email, Integer> {
-}
