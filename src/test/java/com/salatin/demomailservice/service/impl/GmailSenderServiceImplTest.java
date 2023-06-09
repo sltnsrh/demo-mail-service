71dde5b1f2964d68cd90721d0ee1b66bc88d6430
Serhii Salatin <lifenjoy8888@gmail.com>
2023-06-09 12:25:22 +0300

    on GmailSenderServiceImpl send method test was created

---
+++ src/test/java/com/salatin/demomailservice/service/impl/GmailSenderServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.salatin.demomailservice.service.impl;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@ExtendWith(value = MockitoExtension.class)
+class GmailSenderServiceImplTest {
+    @InjectMocks
+    private GmailSenderServiceImpl gmailSenderService;
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    void sendWhenValidDataThenSendMethodInvoke() {
+        doNothing().when(javaMailSender).send(any(SimpleMailMessage.class));
+
+        gmailSenderService.send("to", "subject", "body");
+
+        verify(javaMailSender).send(any(SimpleMailMessage.class));
+    }
+}
