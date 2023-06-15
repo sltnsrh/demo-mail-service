cdfef410504439f73a0af1ce0d6c48b9f41011ec
Serhii Salatin <lifenjoy8888@gmail.com>
2023-06-15 17:25:32 +0300

    added spring doc description for the MailController

--- src/main/java/com/salatin/demomailservice/controller/MailController.java
+++ src/main/java/com/salatin/demomailservice/controller/MailController.java
@@ -2,6 +2,10 @@ package com.salatin.demomailservice.controller;
 
 import com.salatin.demomailservice.service.MailingService;
 import com.salatin.demomailservice.service.Scheduler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,28 +16,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/mail")
 @RequiredArgsConstructor
+@Tag(name = "Mailing", description = "Operations for sending and scheduling emails")
 public class MailController {
     private final MailingService mailingService;
     private final Scheduler mailingJobScheduler;
 
-    @PostMapping
-    @RequestMapping("/users/{userId}/send")
+    @Operation(summary = "Send email to user by ID",
+            description = "Sends single email to the user specified by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Email sent successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
+    @PostMapping("/users/{userId}/send")
     public ResponseEntity<Void> sendRestEmailToUserById(@PathVariable Integer userId) {
         mailingService.sendRestEmailToUserById(userId);
 
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping
-    @RequestMapping("/crons/{cronId}/schedule")
+    @Operation(summary = "Schedule mail job by cron ID",
+            description = "Schedules a mail job based on the specified cron ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Mail job scheduled successfully"),
+            @ApiResponse(responseCode = "202",
+                    description = "Cron job already scheduled, needs to stop previous"),
+            @ApiResponse(responseCode = "404", description = "Cron not found")
+
+    })
+    @PostMapping("/crons/{cronId}/schedule")
     public ResponseEntity<Void> scheduleMailJobByCronId(@PathVariable Integer cronId) {
         mailingJobScheduler.schedule(cronId);
 
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping
-    @RequestMapping("/stop-scheduler")
+    @Operation(summary = "Stop the mailing scheduler",
+            description = "Stops the actually scheduled mailing task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Mailing scheduler stopped successfully"),
+            @ApiResponse(responseCode = "202",
+                    description = "Cron job wasn't created yet or already stopped")
+    })
+    @PostMapping("/stop-scheduler")
     public ResponseEntity<Void> stopScheduledTask() {
         mailingJobScheduler.stop();
 
